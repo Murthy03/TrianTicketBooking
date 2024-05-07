da1b691d3ddc53f89ac0e9e5b56475f1699c19cb
Murthy03 <112764285+Murthy03@users.noreply.github.com>
2024-05-07 10:04:23 +0530

    Add files via upload

---
+++ AuthFilter.java
@@ -0,0 +1,44 @@
+package TTBooking;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = { "/TicketBooking.html", "/ticket.jsp" })
+public class AuthFilter implements Filter {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpSession session = request.getSession(false);
+		if (session != null && session.getAttribute("user") != null) {
+			chain.doFilter(req, res);
+			return;
+		} else {
+			response.sendRedirect("login.html");
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	// Other methods of the Filter interface (init, destroy) can be left empty.
+}
